f1e0fefee4746d2bff96f7972a1ad936f1b2d3e0
Yannick Kagou <yannickkagou@gmail.com>
2024-10-24 14:09:59 +0100

    fix: adding localhost on ALLOWED_HOST and fixing the Subscriber serializer

--- src/music/serializers.py
+++ src/music/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 
+from core.serializers import UserSerializer
+
 from .models import Subscriber
 
 
 class SubscriberSerializer(serializers.ModelSerializer):
-    user_id = serializers.IntegerField(read_only=True)
+    # user_id = serializers.IntegerField(read_only=True)
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Subscriber
-        fields = ['id', 'user_id', 'birth_date']
+        fields = ['id', 'user', 'birth_date']
